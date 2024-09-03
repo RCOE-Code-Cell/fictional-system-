bc59a3f1e201fe4df5a940a70ebfbebd3b900b92
Fareed <fareedsayed95@gmail.com>
2024-09-03 22:08:05 +0530

    updated the sh file

--- github-api/list_users.sh
+++ github-api/list_users.sh
@@ -12,7 +12,7 @@ REPO_OWNER=$1
 REPO_NAME=$2
 
 # Function to make a GET request to the GitHub API
-function github_api_get {
+github_api_get() {
     local endpoint="$1"
     local url="${API_URL}/${endpoint}"
 
@@ -21,7 +21,7 @@ function github_api_get {
 }
 
 # Function to list users with read access to the repository
-function list_users_with_read_access {
+list_users_with_read_access() {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
     # Fetch the list of collaborators on the repository
@@ -37,6 +37,5 @@ function list_users_with_read_access {
 }
 
 # Main script
-
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
