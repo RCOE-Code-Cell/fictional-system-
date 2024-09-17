e20ceec46ea5425b7aa2e586ebd9f7ed5025112d
Fareed95 <fareedsayed95@gmail.com>
2024-09-17 21:26:59 +0530

    updated the terraform file

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -13,12 +13,11 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform_Demo"
-  }
+# Create a new host with instance type of c5.18xlarge with Auto Placement
+# and Host Recovery enabled.
+resource "aws_ec2_host" "test" {
+  instance_type     = "c5.18xlarge"
+  availability_zone = "us-west-2a"
+  host_recovery     = "on"
+  auto_placement    = "on"
 }
-
